872541c5e3a6b825624110f79f48ca17dcdb0ed9
Fiell75785 <Alexandr.zub@mail.ru>
2025-05-19 18:01:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MaxValueFinder
+{
+    public class ArrayProcessor
+    {
+        private int[] _array;
+        public ArrayProcessor(int[] array)
+        {
+            _array = array;
+        }
+        public int FindMax()
+        {
+            if (_array == null || _array.Length == 0)
+                throw new InvalidOperationException("Массив пуст или не инициализирован.");
+            int max = _array[0];
+            for (int i = 1; i < _array.Length; i++)
+            {
+                if (_array[i] > max)
+                {
+                    max = _array[i];
+                }
+            }
+            return max;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = { 4, 7, 1, 15, 3 };
+            ArrayProcessor processor = new ArrayProcessor(array);
+            int max = processor.FindMax();
+            Console.WriteLine("Максимальное значение: " + max);
+        }
+    }
+}
